f864edec232e2f8b9d29b9c1d9e237e7f2bc5a82
Melvis-Bwoma <melvismabera@gmail.com>
2023-02-18 00:04:55 -0800

    g

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i= 'a'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
