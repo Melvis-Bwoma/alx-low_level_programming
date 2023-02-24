8eec4363f315f6d163e19d2c60efc12204d6e5ff
Melvis-Bwoma <melvismabera@gmail.com>
2023-02-23 22:00:38 -0800

    k

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
- * print_numbers -  checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * print_numbers - print numbers since 0 upto 9
+ * exclude 2 and 4
+ * Return: the numbers since 0 to 9
  */
 
 void print_numbers(void)
 {
-	int c;
-
-	for (c = '0'; c <= '9'; c++)
-	for (c = 48; c < 58; c++)
-	{
-	_putchar(c);
-	}
+int x = 0;
+for (; x <= 9; x++)
+{
+if (x == 2 || x == 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
 }
