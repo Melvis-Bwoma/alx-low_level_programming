eafffac99f72c8ce3a045142ecc1cfc370dcdbc2
Melvis-Bwoma <melvismabera@gmail.com>
2023-02-28 11:10:42 -0800

    h

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,7 @@
  * Return: word
  */
 
-void rev_string(char *)
+void rev_string(char *s)
 {
 	int len = 0, index = 0;
 	char tmp;
@@ -14,10 +14,10 @@ void rev_string(char *)
 	while (s[index++])
 		len++;
 
-	for (index = len -1; index >= len / 2; index--)
+	for (index = len - 1; index >= len / 2; index--)
 	{
 		tmp = s[index];
-		s[index] = s[len - index -1];
-		s[len -index -1] = tmp;
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
-::}
+}
