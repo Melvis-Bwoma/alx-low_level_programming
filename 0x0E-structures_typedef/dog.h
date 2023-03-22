565a03f45fac3b2b9c91b2d77e72ad0bcf4c4395
Melvis-Bwoma <melvismabera@gmail.com>
2023-03-21 22:34:08 -0700

    my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+struct dog myDog = { "Rufus", 3.5, "John Smith" };
+{
+	char* name;
+	float age;
+	char* owner;
+}
